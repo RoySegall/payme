ced8f7cad87687907bc68925b4a8c0117b9baee9
Roy Segall <roy@segall.io>
2019-01-25 13:58:37 +0200

    Adding travis CI integration

---
+++ tests/HelloTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class HelloTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
