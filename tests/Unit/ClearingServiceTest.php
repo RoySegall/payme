b81438da59f449d5dc0d04009b5368b7024ef2b1
Roy Segall <roy@segall.io>
2019-01-28 23:27:53 +0200

    Making sure we got results in the DB

--- tests/Unit/ClearingServiceTest.php
+++ tests/Unit/ClearingServiceTest.php
@@ -2,13 +2,15 @@
 
 namespace Tests\Unit;
 
+use App\SalesInformation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\MockTrait;
 use Tests\TestCase;
 
 class ClearingServiceTest extends TestCase
 {
 
-    use MockTrait;
+    use MockTrait, RefreshDatabase;
 
     /**
      * @var \App\Services\ClearingService
@@ -34,7 +36,7 @@ class ClearingServiceTest extends TestCase
         $results = $this->clearingService->paymentRequest(200, 200, 200);
 
         $this->assertFalse($results);
-        $this->assertEquals($this->clearingService->getLog(), (object)[
+        $this->assertEquals($this->clearingService->getLog(), (object) [
             'status_code' => 1,
             'status_error_details' => 'Invalid price',
             'status_additional_info' => 123,
@@ -43,17 +45,13 @@ class ClearingServiceTest extends TestCase
 
         $results = $this->clearingService->paymentRequest(200, 200, 200);
 
-        $this->assertEquals($this->clearingService->getLog(), (object) [
-            'status_code' => 0,
-            'sal_url' => 'http://google.com',
-            'payme_sale_id' => 123,
-            'payme_sale_code' => 352,
-            'price' => 352,
-            'transaction_id' => 352,
-            'currency_id' => 352,
-        ]);
+        $log = $this->clearingService->getLog();
+
+        $this->assertEquals($this->clearingService->getLog(), (object) $log);
         $this->assertTrue($results);
 
-        // todo: test DB integration.
+        // Checking the sale information logged to the DB.
+        $query_results = SalesInformation::where('payme_sale_code', $log->payme_sale_code)->first();
+        $this->assertNotEmpty($query_results);
     }
 }
